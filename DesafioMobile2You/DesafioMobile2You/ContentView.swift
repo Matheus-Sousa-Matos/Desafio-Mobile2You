8802f0fc40b794f11b9ed466be9177970140c2f5
Matheus de Sousa Matos <69324938+Matheus-Sousa-Matos@users.noreply.github.com>
2022-07-29 11:21:37 -0300

    Add files via upload

---
+++ DesafioMobile2You/DesafioMobile2You/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesafioMobile2You
+//
+//  Created by Matheus de Sousa Matos on 29/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DesafioMobile2You/DesafioMobile2You/DesafioMobile2YouApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesafioMobile2YouApp.swift
+//  DesafioMobile2You
+//
+//  Created by Matheus de Sousa Matos on 29/07/22.
+//
+
+import SwiftUI
+
+@main
+struct DesafioMobile2YouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
